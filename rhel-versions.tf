859508a906f1b0d4bae071c4d142ae86cf325bb3
James Blair <mail@jamesblair.net>
2022-10-31 14:47:34 +1300

    Refreshed list of rhel ami's, added rhel 9.

--- rhel-versions.tf
+++ rhel-versions.tf
@@ -2,15 +2,15 @@
 # Get latest RHEL AMI with Terraform #
 ######################################
 
-# RHEL 8.5
-data "aws_ami" "rhel_8_5" {
+# RHEL 9.0
+data "aws_ami" "rhel_8_6" {
   most_recent = true
 
   owners = ["309956199498"] // Red Hat's Account ID
 
   filter {
     name   = "name"
-    values = ["RHEL-8.5*"]
+    values = ["RHEL-9.0*"]
   }
 
   filter {
@@ -29,42 +29,15 @@ data "aws_ami" "rhel_8_5" {
   }
 }
 
-# RHEL 8.4
-data "aws_ami" "rhel_8_4" {
+# RHEL 8.6
+data "aws_ami" "rhel_8_6" {
   most_recent = true
 
   owners = ["309956199498"] // Red Hat's Account ID
 
   filter {
     name   = "name"
-    values = ["RHEL-8.4*"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-# RHEL 8.3
-data "aws_ami" "rhel_8_3" {
-  most_recent = true
-
-  owners = ["309956199498"] // Red Hat's Account ID
-
-  filter {
-    name   = "name"
-    values = ["RHEL-8.3*"]
+    values = ["RHEL-8.6*"]
   }
 
   filter {
@@ -93,60 +66,6 @@ data "aws_ami" "rhel_7_9" {
   filter {
     name   = "name"
     values = ["RHEL-7.9*"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-# RHEL 7.8
-data "aws_ami" "rhel_7_8" {
-  most_recent = true
-
-  owners = ["309956199498"] // Red Hat's Account ID
-
-  filter {
-    name   = "name"
-    values = ["RHEL-7.8*"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-# RHEL 7.7
-data "aws_ami" "rhel_7_7" {
-  most_recent = true
-
-  owners = ["309956199498"] // Red Hat's Account ID
-
-  filter {
-    name   = "name"
-    values = ["RHEL-7.7*"]
   }
 
   filter {
