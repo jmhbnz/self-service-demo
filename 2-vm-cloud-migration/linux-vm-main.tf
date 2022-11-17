a430bd941ad93a6a0582950b8f21e04ae4a7adbf
James Blair <mail@jamesblair.net>
2022-11-18 08:36:13 +1300

    Ensure port 443 is allowed ingress in the security group.

--- 2-vm-cloud-migration/linux-vm-main.tf
+++ 2-vm-cloud-migration/linux-vm-main.tf
@@ -69,6 +69,14 @@ resource "aws_security_group" "aws-linux-sg" {
   }
 
   ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming HTTPS connections"
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
--- 2-vm-cloud-migration/network-main.tf
+++ 2-vm-cloud-migration/network-main.tf
@@ -2,7 +2,7 @@
 ## Network Single AZ Public Only - Main ##
 ##########################################
 
-# Create the VPC
+# Create the vpc
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
