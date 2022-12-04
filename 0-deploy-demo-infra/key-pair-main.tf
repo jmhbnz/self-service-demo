eb8c71b2237515c3adbd68a955ff9b92ab6489f6
James Blair <mail@jamesblair.net>
2022-12-05 07:59:09 +1300

    Ensure key is written with secure mode.

--- 0-deploy-demo-infra/key-pair-main.tf
+++ 0-deploy-demo-infra/key-pair-main.tf
@@ -16,6 +16,7 @@ resource "aws_key_pair" "key_pair" {
 
 # Save file
 resource "local_file" "ssh_key" {
-  filename = "${aws_key_pair.key_pair.key_name}.pem"
-  content  = tls_private_key.key_pair.private_key_pem
+  filename        = "${aws_key_pair.key_pair.key_name}.pem"
+  content         = tls_private_key.key_pair.private_key_pem
+  file_permission = 0644
 }
