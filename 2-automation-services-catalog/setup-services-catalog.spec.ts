982b0a777b1d2fedd3b3b8d608bb39cbdb0aa937
James Blair <mail@jamesblair.net>
2023-01-16 14:35:01 +1300

    Added new module to automate service catalog setup with playwright.

---
+++ 2-automation-services-catalog/setup-services-catalog.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+// We are using self signed certificates for our demo environment
+test.use({
+  ignoreHTTPSErrors: true
+});
+
+test('test', async ({ page }) => {
+
+    // Open the automation service catalog dashboard
+    // This will automatically start the oauth flow and redirect to keycloak
+    await page.goto('https://catalog.rhdemo.win');
+    await expect(page.locator("#kc-page-title")).toBeVisible()
+
+    // Authenticate using environment vars
+    await page.getByLabel('Username or email').click();
+    await page.getByLabel('Username or email').fill(process.env.catalog_user);
+    await page.getByLabel('Username or email').press('Tab');
+    await page.getByLabel('Password').fill(process.env.catalog_password);
+    await page.getByLabel('Password').press('Enter');
+
+    // Create a portfolio for non production
+    await page.getByRole('link', { name: 'Portfolios' }).click();
+    await page.locator('body').click();
+    await page.getByRole('button', { name: 'Create' }).click();
+    await page.locator('input[name="name"]').click();
+    await page.locator('input[name="name"]').fill('Middleware non-prod');
+    await page.getByLabel('Description').click();
+    await page.getByLabel('Description').fill('All products for middleware non-prod environments');
+    await page.getByRole('contentinfo').getByRole('button', { name: 'Create' }).click();
+
+    // Create a product for a developer environment
+    await page.getByRole('button', { name: 'Add' }).click();
+    await page.getByRole('button', { name: 'open menu' }).click();
+    await page.getByRole('option', { name: 'Automation Controller' }).click();
+    await page.locator('[id="\\33 "]').check();
+    await page.getByTitle('Add').click();
+});
